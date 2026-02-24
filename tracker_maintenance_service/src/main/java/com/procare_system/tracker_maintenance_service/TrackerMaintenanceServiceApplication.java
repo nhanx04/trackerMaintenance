d20e7b4225ed4a82fd485b268a689b1565f8f5d1
CanHoangHa <canhoangha2005@gmail.com>
2026-02-24 20:33:09 +0700

    Init backend service

---
+++ tracker_maintenance_service/src/main/java/com/procare_system/tracker_maintenance_service/TrackerMaintenanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.procare_system.tracker_maintenance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerMaintenanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerMaintenanceServiceApplication.class, args);
+	}
+
+}
---
+++ tracker_maintenance_service/src/test/java/com/procare_system/tracker_maintenance_service/TrackerMaintenanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procare_system.tracker_maintenance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerMaintenanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
