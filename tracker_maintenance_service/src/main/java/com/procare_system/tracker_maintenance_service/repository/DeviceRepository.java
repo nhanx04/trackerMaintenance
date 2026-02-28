f3523ea8311e422f7dc995e22a6f67966c630aad
CanHoangHa <canhoangha2005@gmail.com>
2026-02-28 14:25:12 +0700

    Fix id type in DeviceRepository

--- tracker_maintenance_service/src/main/java/com/procare_system/tracker_maintenance_service/repository/DeviceRepository.java
+++ tracker_maintenance_service/src/main/java/com/procare_system/tracker_maintenance_service/repository/DeviceRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DeviceRepository extends JpaRepository<Device, String>, JpaSpecificationExecutor<Device> {
+public interface DeviceRepository extends JpaRepository<Device, Long>, JpaSpecificationExecutor<Device> {
     boolean existsByCode(String code);
 }
